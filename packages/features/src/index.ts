a4a14fc0e29c9c3571112e236188d9559b2351ba
baluwashere <113037190+baluwashere@users.noreply.github.com>
2026-09-19 11:24:23 +0100

    T4: export feature engine API

---
+++ packages/features/src/index.ts
@@ -0,0 +1,12 @@
+export { FeatureEngine, type FeatureEngineOptions } from './engine.js';
+export {
+  FEATURE_NAMES,
+  type FeatureAvailability,
+  type FeatureFeedName,
+  type FeatureFeedStatus,
+  type FeatureMarketSnapshot,
+  type FeatureName,
+  type FeatureUnavailableReasons,
+  type FeatureValues,
+  type FeatureVector,
+} from './types.js';
